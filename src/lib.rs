43df9f565b454535cb5086ab8684422c01ced134
Julian Kenwood <nomad010@gmail.com>
2018-10-21 17:38:29 +0200

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use std::collections::{HashMap, hash_map::{Iter as HashMapIter, RandomState}};
+use std::collections::{HashMap, hash_map::{Entry, Iter as HashMapIter, RandomState}};
 use std::hash::{Hash, BuildHasher};
 use std::iter::{ExactSizeIterator, FusedIterator};
+use std::borrow::Borrow;
 
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct MultiHashSet<T: Eq + Hash, S : BuildHasher = RandomState> {
     values: HashMap<T, usize, S>,
     total_size: usize,
@@ -60,7 +61,103 @@ impl<T: Eq + Hash, S: BuildHasher> MultiHashSet<T, S> {
         Iter::new(self.values.iter(), self.total_size)
     }
 
+    /*
+    Iterator stuff here
 
+    pub fn difference<'a>(
+    &'a self, 
+    other: &'a HashSet<T, S>
+) -> Difference<'a, T, S>
+
+pub fn symmetric_difference<'a>(
+    &'a self, 
+    other: &'a HashSet<T, S>
+) -> SymmetricDifference<'a, T, S>
+
+pub fn intersection<'a>(
+    &'a self, 
+    other: &'a HashSet<T, S>
+) -> Intersection<'a, T, S>
+
+pub fn union<'a>(&'a self, other: &'a HashSet<T, S>) -> Union<'a, T, S>
+    */
+
+    pub fn len(&self) -> usize {
+        self.total_size
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.values.is_empty()
+    }
+
+    /*
+    pub fn drain(&mut self) -> Drain<T>
+    */
+
+    pub fn clear(&mut self) {
+        self.total_size = 0;
+        self.values.clear()
+    }
+
+    pub fn contains<Q: ?Sized>(&self, value: &Q) -> bool
+        where T: Borrow<Q>,
+              Q: Hash + Eq
+    {
+        self.values.contains_key(value)
+    }
+
+    pub fn get_count<Q: ?Sized>(&self, value: &Q) -> usize
+        where T: Borrow<Q>,
+              Q: Hash + Eq
+    {
+        *self.values.get(value).unwrap_or(&0)
+    }
+
+    pub fn is_disjoint(&self, other: &MultiHashSet<T, S>) -> bool {
+        for ref item in self.iter() {
+            if other.contains(&item) {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn is_subset(&self, other: &MultiHashSet<T, S>) -> bool {
+        for ref item in self.iter() {
+            if !other.contains(&item) {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn is_superset(&self, other: &MultiHashSet<T, S>) -> bool {
+        other.is_subset(self)
+    }
+
+    pub fn insert(&mut self, value: T) -> bool {
+        match self.values.entry(value) {
+            Entry::Occupied(o) => { let x = o.into_mut(); *x += 1; false }
+            Entry::Vacant(v) => {v.insert(0); true }
+        }
+    }
+
+    pub fn remove<Q: ?Sized>(&mut self, value: &Q) -> bool 
+        where T: Borrow<Q>,
+              Q: Hash + Eq
+    {
+        match self.values.entry(value.borrow()) {
+            Entry::Occupied(o) => { 
+                let x = o.into_mut();
+                *x -= 1;
+                if *x == 0 {
+                    o.remove_entry();
+                }
+                true
+            },
+            Entry::Vacant(v) => { false }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -128,6 +225,10 @@ impl<'a, T: 'a> Iterator for Iter<'a, T> {
             }
         }
     }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.len()))
+    }
 }
 
 impl<'a, T: 'a> ExactSizeIterator for Iter<'a, T> {
@@ -139,10 +240,29 @@ impl<'a, T: 'a> ExactSizeIterator for Iter<'a, T> {
 impl<'a, T: 'a> FusedIterator for Iter<'a, T> {
 }
 
-
-#[derive(Debug)]
-struct Difference<'a, T: 'a, S: 'a> {
+/*
+#[derive(Clone, Debug)]
+struct Difference<'a, T: 'a + Hash + Eq, S: 'a + BuildHasher> {
     iter: Iter<'a, T>,
     // the second set
-    other: &'a HashMap<T, usize, S>,
+    other: &'a MultiHashSet<T, S>,
 }
+
+impl<'a, T: Eq + Hash, S: BuildHasher> Iterator for Difference<'a, T, S> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<&'a T> {
+        loop {
+            let elt = self.iter.next()?;
+            if !self.other.contains(elt) {
+                return Some(elt);
+            }
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let (_, upper) = self.iter.size_hint();
+        (0, upper)
+    }
+}
+*/
