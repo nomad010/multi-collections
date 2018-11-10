f14d5ae1f5a8a79240692beac1ce872f181c94f0
Julian Kenwood <nomad010@gmail.com>
2018-11-10 19:33:33 +0200

    WIP

--- src/multi_btreeset.rs
+++ src/multi_btreeset.rs
@@ -14,14 +14,43 @@ pub struct MultiBTreeSet<T: Ord> {
 }
 
 impl<T: Ord> MultiBTreeSet<T> {
+    /// Makes a new `MultiBTreeSet`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #![allow(unused_mut)]
+    /// use std::collections::BTreeSet;
+    ///
+    /// let mut set: MultiBTreeSet<i32> = MultuBTreeSet::new();
+    /// ```
     pub fn new() -> MultiBTreeSet<T> {
         MultiBTreeSet { 
             values: BTreeMap::new(),
             total_size: 0
         }
     }
-}
 
+    pub fn clear(&mut self) {
+        self.total_size = 0;
+        self.values.clear()
+    }
+
+    pub fn get_count<Q: ?Sized>(&self, key: &Q) -> usize
+        where T: Borrow<Q>,
+              Q: Ord
+    {
+        *self.values.get(key).unwrap_or(&0)
+    }
+
+    pub fn contains<Q: ?Sized>(&self, value: &Q) -> bool
+        where T: Borrow<Q>,
+              Q: Ord
+    {
+        self.get_count(value) > 0
+    }
+
+}
 
 #[cfg(test)]
 mod tests {
